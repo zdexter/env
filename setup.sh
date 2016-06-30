53cda9e9390239c8c5386e5b94a36594e55ae7fc
Zach Dexter <zach.dexter@gmail.com>
2016-06-30 10:45:10 -0400

    Don't link non-files. Add output to setup script.

--- setup.sh
+++ setup.sh
@@ -5,6 +5,7 @@
 BREW_PACKAGES='git bash-completion coreutils tmux'
 VIM_PACKAGE='vim --env-std --override-system-vim'
 
+echo "Installing Homebrew packages: $BREW_PACKAGES"
 brew prune
 brew install $BREW_PACKAGES $VIM_PACKAGE
 
@@ -14,6 +15,7 @@ git submodule update --init
 
 BASE=$(greadlink -e $(dirname $0))
 
-for fname in $BASE/userdots/.*; do
+for fname in `ls -A $BASE/userdots/`; do
+    echo "Linking ~/$fname"
     ln -sf `greadlink -e $fname` $HOME/
 done
